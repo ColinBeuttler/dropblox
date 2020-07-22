6959dcfa933b55028ce875ebfac3f3f94a556f70
Colin <beutt102@mail.chapman.edu>
2020-07-22 02:29:29 -0700

    update2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To Dropblox
+          Enter Object
         </p>
+      
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Listarray.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import App from './App';
+import { ReactComponent } from '*.svg';
+
+class listarray extends React.Component
+    
+
+export default listarray;
