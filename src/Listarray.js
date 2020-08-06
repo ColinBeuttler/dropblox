729cbac9b3b980b4bc4401e84abd40f80395ea56
Colin <beutt102@mail.chapman.edu>
2020-08-06 13:48:20 -0700

    ltupdate

--- src/Listarray.js
+++ src/Listarray.js
@@ -1,24 +1,4 @@
-import React, { useState } from 'react';
-<<<<<<< HEAD
-import { ReactComponent } from 'Listarray.js';
-import { render } from '@testing-library/react';
-
-class listarray extends React.Component {
-    state = {
-        data: Array.from.listarray
-    }
-    render() {
-        const { data } = this.state
-        return (
-            <div>
-
-            </div>
-        )
-    }
-};
-=======
-import './App.css';
-import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 
 function Listarray() {
     
@@ -32,11 +12,6 @@ function Listarray() {
         </div>
     )
     
-
-
-
-}
->>>>>>> fb52d70cbce1a8354fd38236f77f9365d1f92a55
-
+};
 
 export default Listarray;
