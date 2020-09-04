7fa03828a57b48850369815b771e8e0c51274db6
Colin Beuttler <beutt102@mail.chapman.edu>
2020-09-04 13:38:50 -0700

    udateeject

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-=======
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Fileselector from './Fileselector'
-import Listfile from './Listfiles';
+import Fileselector from './Containers/Fileselector'
+import Listfile from './Containers/Listfiles';
 
 
 
@@ -51,7 +24,7 @@ class App extends Component {
 
           <Fileselector filename={this.state}/>
 
-          {/* <Listfile /> */}
+          <Listfile />
 
           <img src={logo} className="App-logo" alt="logo" />
 
@@ -68,5 +41,4 @@ class App extends Component {
 
 
 
->>>>>>> 2f0a8811ffd8eb0489cdb542297bacafe3f353a5
 export default App;
---
+++ src/Containers/Fileselector.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class Fileselector extends Component {
+
+
+componentDidMount(){
+    axios.get('https://filedrop-fa092.firebaseio.com/files.json')
+    .then(response => {
+        this.setState({files: response.data})
+    })
+}
+
+
+
+    render() {
+        return (
+            <div className="Fileselector">
+
+                <input className="input" type="file" onChange={this.handleChange}>
+
+                </input>
+
+            </div>
+        )
+    
+
+    }
+
+};
+
+export default Fileselector;
