75cce4f27b528360990edde856397b8d4c78ff35
Colin Beuttler <beutt102@mail.chapman.edu>
2020-11-17 12:54:41 -0800

    updatedk

--- src/Containers/Listfiles.js
+++ src/Containers/Listfiles.js
@@ -1,19 +1,20 @@
-import React from 'react';
-// import React, { useState } from 'react';
+// import React from 'react';
+// // import React, { useState } from 'react';
 // import React, { Component } from 'react';
 
 // class Listfile extends Component {
+
 //     // state = {
 //     //     name: this.file
-//     }
+
 //     render() {
+
 //         return (
 //             <div> {this.state.file} </div>
 //         )
 //     }
 
-
-
-// }
+//     }
+    
 
 // export default Listfile
